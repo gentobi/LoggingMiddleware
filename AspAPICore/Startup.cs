9d60598cdddb02641435b30524611ad3cd66e896
Tan Nguyen <crazytan94@gmail.com>
2019-11-28 20:03:24 +0700

    Clean code.

--- AspAPICore/Startup.cs
+++ AspAPICore/Startup.cs
@@ -1,47 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AspAPICore.Configurations;
+﻿using AspAPICore.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using ILogger = NLog.ILogger;
 
 namespace AspAPICore
 {
-	public class Startup
-	{
-		private readonly ILogger _logger;
-		private IConfiguration Configuration { get; }
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-			_logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-		}
+    public class Startup
+    {
+        private readonly ILogger _logger;
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+        }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddSingleton(_logger);
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_logger);
 
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-		}
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-			app.UseMiddleware<RequestResponseLoggingMiddleware>();
-			app.UseMvc();
-		}
-	}
+            app.UseMiddleware<RequestLoggerMiddleware>();
+            app.UseMvc();
+        }
+    }
 }
